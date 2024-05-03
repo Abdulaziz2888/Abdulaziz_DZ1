ec597ac7428afdd307a287eaee91e2861930492f
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-03 20:39:32 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String multiWordVariable; // 1
+        final int NUM = 10; // 2
+        String word = "Hello"; // 3
+        multiWordVariable = NUM + word; // 4
+        System.out.println("multiWordVariable: " + multiWordVariable);
+        System.out.println("NUM: " + NUM);
+        System.out.println("word: " + word);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+        System.out.println("Введите ваше имя: "); // 7
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine(); // 8
+        System.out.println("Привет, " + userName + "!"); // 9
     }
 }
